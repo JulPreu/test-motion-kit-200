6e1d9ea63fe70644e4639a404d003b9eaf61c0ad
JulPreu <julian.preuschoff@gmx.net>
2025-08-03 09:15:50 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,26 @@
+let start = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        start = 1
+    }
+    if (input.buttonIsPressed(Button.B)) {
+        start = 0
+    }
+    if (start == 1) {
+        // links
+        maqueen.servoRun(maqueen.Servos.S1, 120)
+        if (maqueen.ultrasonic(maqueen.DistanceUnit.Centimeters) < 30) {
+            maqueen.setColor(0xff0000)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 50)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CCW, 50)
+        } else {
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 200)
+            maqueen.writeLED(maqueen.Led.LedAll, maqueen.LedSwitch.LedOn)
+            maqueen.setColor(0x00ffff)
+        }
+        maqueen.servoRun(maqueen.Servos.S1, 60)
+    }
+})
+basic.forever(function () {
+	
+})
